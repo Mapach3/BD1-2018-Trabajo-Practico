464a8558ae80f961318ca457270242e8c96bcffb
Mapach3 <londmt3@gmail.com>
2018-11-06 19:59:47 -0300

    Carga de Clientes y Cabañas

--- source/gallinero_3.1/gallinero_cargaDeDatos_3.1.sql
+++ source/gallinero_3.1/gallinero_cargaDeDatos_3.1.sql
@@ -86,8 +86,8 @@ call alta_calle_has_localidad(4,12);
 
 select * from calle;
 
-/*ver provincias, sus localidades y sus calles respectivas*/
-select provincia.nombre as Provincia,localidad.nombre as Localidad,calle.nombre as Calle from provincia
+/*ver provincias, sus localidades y sus calles respectivas*/ -- usar para cargar los clientes y cabañas
+select provincia.nombre as Provincia,provincia.idProv as idProvin,localidad.nombre as Localidad,localidad.idLocalidad as IdLocal,calle.nombre as Calle,calle.Idcalle as idCalle from provincia
 inner join provincia_has_localidad on
 provincia.idProv=provincia_has_localidad.provincia_idprov
 inner join localidad on
@@ -99,6 +99,67 @@ calle.idCalle=calle_has_localidad.calle_idCalle;
 
 
 /*Fin de carga de Provincias, Localidades y Calles*/
+
+
+/*carga de clientes*/
+
+call alta_clientes("Ricardo Perez","20-9928134-12",33,6,1,1);
+call alta_clientes("Mario Herrera","20-2111232-14",50,7,2,1);
+call alta_clientes("Tomas Jones","15-22134124-12",2134,4,3,1);
+call alta_clientes("Mariela Mariluz","18-23122452-19",1234,3,4,1);
+
+call alta_clientes("Amparo Fernandez","15-7772134-14",500,5,5,2);
+call alta_clientes("Juana Repetto","20-8823434-14",2377,5,6,2);
+call alta_clientes("Juan Flores","15-4232134-14",372,6,7,2);
+call alta_clientes("Mariluz Fernandez","22-2222222-14",9923,7,8,2);
+
+
+call alta_clientes("Juanito Alegria","14-7772134-14",233,11,10,3);
+call alta_clientes("Ignacio Montaña","20-1234567-14",2377,8,11,3);
+call alta_clientes("Jose Andrea","15-231111-14",372,12,10,3);
+call alta_clientes("Esteban Chimbo","22-6666666-14",372,12,10,3); #test para ver si puede haber 2 con la misma direccion
+
+select * from clientes;
+/*fin de carga de clientes*/
+
+
+/*carga de cabañas*/
+
+call alta_cabaña("Los Pollos Hermanos SRL",1,"20-99999-28",600,3,4,1);
+call alta_cabaña("La Polerria SA",2,"20-6627134-28",610,6,1,1);
+call alta_cabaña("La Querencia",3,"20-1234-39",421,7,2,1);
+call alta_cabaña("Codigo POLLO",4,"20-9922713-21",382,5,6,2);
+call alta_cabaña("Don Mario",5,"20-1234-39",991,6,7,2);
+call alta_cabaña("Abuelo Juan",6,"20-98765-14",239,3,4,2);
+
+select * from cabaña;
+
+/*fin de carga de cabañas*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
